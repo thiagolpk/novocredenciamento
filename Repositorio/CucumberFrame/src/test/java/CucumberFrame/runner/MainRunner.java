90c87eed5b8bbd2d9f4d7d86b416d252efa96255
Thiago Silva <thiago.comth@hotmail.com>
2018-03-08 16:42:28 -0300

    teste de alteração

--- Repositorio/CucumberFrame/src/test/java/CucumberFrame/runner/MainRunner.java
+++ Repositorio/CucumberFrame/src/test/java/CucumberFrame/runner/MainRunner.java
@@ -13,7 +13,7 @@ import cucumber.api.junit.Cucumber;
 	plugin = {//"pretty", "html:target/cucumber", 
 		//	"json:target/cucumber.json", 
 			"com.cucumber.listener.ExtentCucumberFormatter:target/Resultado.html"}
-	, tags = {"@MARABERTO", "@CPF"}
+	//, tags = {"@MARABERTO", "@CPF"}
 )
 public class MainRunner {
 
